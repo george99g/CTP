3879090bd9af7c246be80f936a7cdf47cdf53864
george99g <georgi_99g@abv.bg>
2016-02-24 09:22:44 +0200

    Error handling messages.

--- CTP-client/mainwindow.cpp
+++ CTP-client/mainwindow.cpp
@@ -74,6 +74,26 @@ void MainWindow::handleSocketReadyRead()
         if(messageParameters.at(0) == "MY_CHANNELLIST" && messageParameters.count() > 1)
             for(unsigned i = 1; i < (unsigned)messageParameters.count(); i++)
                 requestUsernamesForChannel(messageParameters.at(i));
+        else if(messageParameters.at(0) == "CHANNEL_DOES_NOT_EXIST")
+            QMessageBox::warning(this, tr("warning.channelDoesNotExist"), tr("warning.channelDoesNotExist.text"));
+        else if(messageParameters.at(0) == "USER_DOES_NOT_EXIST")
+            QMessageBox::warning(this, tr("warning.userDoesNotExist"), tr("warning.userDoesNotExist.text"));
+        else if(messageParameters.at(0) == "WRONG_ARGUMENTS")
+            QMessageBox::warning(this, tr("warning.wrongParameters"), tr("warning.wrongParameters.text"));
+        else if(messageParameters.at(0) == "USER_IS_NOT_LOGGED_IN" && messageParameters.count() > 1)
+            QMessageBox::warning(this, tr("warning.userIsNotLoggedIn"), tr("warning.userIsNotLoggedIn.text:%1").arg(messageParameters.at(1)));
+        else if(messageParameters.at(0) == "NOT_IN_CHANNEL" && messageParameters.count() > 1)
+            QMessageBox::warning(this, tr("warning.notInChannel"), tr("warning.notInChannel.text:%1").arg(messageParameters.at(1)));
+        else if(messageParameters.at(0) == "NOT_ADMINISTRATOR")
+            QMessageBox::warning(this, tr("warning.notAdministrator"), tr("warning.notAdministrator.text"));
+        else if(messageParameters.at(0) == "ALREADY_JOINED" && messageParameters.count() > 1)
+            QMessageBox::warning(this, tr("warning.alreadyInChannel"), tr("warning.alreadyInChannel.text:%1").arg(messageParameters.at(1)));
+        else if(messageParameters.at(0) == "CHANNEL_MODE_DOES_NOT_ALLOW_ACCESS")
+            QMessageBox::warning(this, tr("warning.notAllowedInChannel"), tr("warning.notAllowedInChannel.text"));
+        else if(messageParameters.at(0) == "QUERY_ERROR")
+            QMessageBox::warning(this, tr("warning.queryError"), tr("warning.queryError.text"));
+        else if(messageParameters.at(0) == "NOT_LOGGED_IN")
+            handleSocketDisconnected();
     }
     return;
 }
